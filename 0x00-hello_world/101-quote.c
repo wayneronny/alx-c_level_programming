da27b65be35a982f9704c83500ca7f79b06d3c7f
wayneronny <itsronaldomondi@gmail.com>
2023-07-13 18:28:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line to the standard error
- * Return 1 (success)
+ *
+ * Description: This program prints a quote from Dora Korpar.
+ *
+ * Return: 1 (success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+  fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+  return (1);
 }
