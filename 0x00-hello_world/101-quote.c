9e8a31320900a77287ff49861f2b9ee56932f168
wayneronny <itsronaldomondi@gmail.com>
2023-07-13 17:46:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
  * main - a program that prints a line to a standard error
- * Return= 0 (success)
+ * Return= 1 (success)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful"- Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 return (1);
 }
 
