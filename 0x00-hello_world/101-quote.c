dc0f4943f922a8443df04e41982a2e225e713ffc
wayneronny <itsronaldomondi@gmail.com>
2023-07-13 18:22:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
- * Return 0 (success)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (success)
  */
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (0);
+return (1);
 }
