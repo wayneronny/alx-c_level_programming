8b677a56de99b76260abbfddd8f9f851df736674
wayneronny <itsronaldomondi@gmail.com>
2023-07-13 18:31:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - A c program that prints a line to the standard error
  *
@@ -10,6 +9,6 @@
  */
 int main(void)
 {
-  fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-  return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
